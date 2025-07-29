c5f554e3ceb80d62f4c781b1c99677e90cc809cd
whirlwindnoa <whirlnoa@proton.me>
2025-07-29 17:34:43 +0400

    fix

--- src/index.js
+++ src/index.js
@@ -20,8 +20,6 @@ import https from "https";
 import crypto from "crypto";
 import { PassThrough } from "stream";
 
-import { merge } from "./combine.js";
-
 program
   .option(
     "-d, --date <date...>",
